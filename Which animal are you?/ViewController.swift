0135b7bf51715edf6466f7bb11d362601edbb702
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-11-08 13:34:21 +0300

    Initial Commit

---
+++ Which animal are you?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Which animal are you?
+//
+//  Created by Дмитрий on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
