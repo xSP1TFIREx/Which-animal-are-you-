602c1e717d5cf7e9e9ee9b4c0d1fca73ed75cfb5
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-11-08 21:37:25 +0300

    Questions logic added

--- Which animal are you?/ViewControllers/ViewController.swift
+++ Which animal are you?/ViewControllers/ViewController.swift
@@ -1,17 +1,12 @@
-//
-//  ViewController.swift
-//  Which animal are you?
-//
-//  Created by Дмитрий on 08.11.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    private var questions = Question.getQuestions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
 
 
